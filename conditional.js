21478bafc6f2d5f1f829791f2cbf3ff391140aeb
Ipineda <irvin.pineda024@gmail.com>
2025-02-18 09:10:14 -0600

    Add conditional example :sparkles:

---
+++ conditional.js
@@ -0,0 +1,10 @@
+let num = prompt("Ingresa tu edad");
+num = Number(num);
+
+if (num >= 18) {
+    alert("Es mayor de edad")
+} else if (num == 15) {
+    alert("Ta chiquito y tiene 15")
+} else {
+    alert("Ta chiquito ")
+}
